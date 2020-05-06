63b855aede7e55a7f655b691fbc3f79ade002b57
Nick Jantz <thenickjantz@gmail.com>
2020-05-06 15:08:05 -0500

    add chess board feature

--- app/components/App.js
+++ app/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import {connect} from 'react-redux'
+import Chessboard from 'chessboardjsx';
 
 import {getPlayerData} from '../actions';
 
 import {getDate} from '../utils/time';
+import Board from './Board';
 
 class App extends React.Component {
 
@@ -20,7 +22,8 @@ class App extends React.Component {
 		return (
             <React.Fragment>
                 <h1>Chess Analytics App</h1>
-                <button onClick={() => this.handleClick}>CLICK ME!</button>}
+                <button onClick={() => this.handleClick}>CLICK ME!</button>
+                <Board />
             </React.Fragment>
 		)
 	}
@@ -30,7 +33,7 @@ const mapStateToProps = (state) => {
 	return {
         game: state.game,
         games: state.games,
-        joinedDate: getDate(state.game.joined)
+        // joinedDate: getDate(state.game.joined)
     }
 };
 
---
+++ app/components/Board.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Chessboard from 'chessboardjsx';
+
+class Board extends React.Component {
+
+    render() {
+
+        return (
+            <Chessboard position="start"/>
+        )
+    }
+
+}
+
+export default Board
